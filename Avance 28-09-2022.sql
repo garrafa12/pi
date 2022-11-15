c6d005d0e4323aa3b4f78c754d779a8b33e09288
garrafa12 <losfafas.sa@gmail.com>
2022-11-15 16:34:54 -0300

    Carga de archivos

---
+++ Avance 28-09-2022.sql
@@ -0,0 +1,14 @@
+select * from Tipo_libro;
+select * from Editorial;
+select * from Persona;
+select * from Tipo_persona;
+select * from Libro_editorial;
+select * from Existencia;
+
+
+
+insert into Libro_editorial (cod_tipo_editorial,Cod_libro,Cod_editorial)
+values(1,'','');
+
+insert into Existencia(cantidad)
+values(5);
